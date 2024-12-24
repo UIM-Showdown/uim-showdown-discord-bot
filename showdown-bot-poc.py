20680e992a86e35329531d9f1a9df6ff41f0ac2c
kuhnertdm <kuhnertdm@gmail.com>
2024-12-24 16:14:57 -0500

    Remove todo

--- showdown-bot-poc.py
+++ showdown-bot-poc.py
@@ -7,7 +7,6 @@ parser = argparse.ArgumentParser(
   prog = 'Showdown Bot POC',
   description = 'POC for the UIM Showdown bot'
 )
-# TODO add descriptions for these
 parser.add_argument('--updatecommands', action='store_true', help='Updates command list on the Discord server - DO NOT SPAM THIS OR YOU WILL BE RATE LIMITED')
 parser.add_argument('--updatesignuproles', action="store_true", help='Assigns "Signup" and "Competitor" roles and exits')
 parser.add_argument('--setupserver', action='store_true', help='Creates team roles/categories/channels and assigns roles to players, and exits')
